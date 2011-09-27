1765d9b28f127f932b92a5eb311c7eae687d8a2b
Samuel L <gsam101@gmail.com>
2011-09-27 14:28:48 +0200

    Premier commit

---
+++ include/Converter.h
@@ -0,0 +1,14 @@
+#ifndef CONVERTER_H
+#define CONVERTER_H
+#include <QtCore/QObject>
+/**
+  * Gère la conversion des images
+  * @brief Gère la conversion des images
+  * @author Samuel L
+  */
+class Converter : public QObject
+{
+
+};
+
+#endif // CONVERTER_H
