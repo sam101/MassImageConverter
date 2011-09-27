f70b1951d725baebf54c29bd57a1c7454773ace5
Samuel L <gsam101@gmail.com>
2011-09-27 15:00:38 +0200

    Ajouts dans MainWindow

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,8 +1,15 @@
-#include "MainWindow.h"
-
+#include <MainWindow.h>
+#include <QtGui/QVBoxLayout>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    setWindowTitle(tr("MassImageConverter 1.0"));
+    //On construit le layout de la fenêtre.
+    QVBoxLayout *layout = new QVBoxLayout;
+    QWidget *widget = new QWidget;
+    setCentralWidget(widget);
+    widget->setLayout(layout);
+    //On ajoute
 }
 
 MainWindow::~MainWindow()
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,12 @@
 #include <QtGui/QApplication>
-#include "MainWindow.h"
+#include <MainWindow.h>
 #include <Converter.h>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QStringList list;
-    list << "image.JPG";
-
-    Converter c;
-    c.start(list,QSize(1000,1000));
+    MainWindow w;
+    w.show();
 
     return a.exec();
 }
